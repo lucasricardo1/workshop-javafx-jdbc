2179cc1a00d848a9fa41fe52e527d49b0bab6e24
Lucas Ricardo <lucasricardox.o@gmail.com>
2022-09-03 00:36:32 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -112,6 +112,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.showAndWait();
 
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loadgin view", e.getMessage(), AlertType.ERROR);
 		}
 	}
